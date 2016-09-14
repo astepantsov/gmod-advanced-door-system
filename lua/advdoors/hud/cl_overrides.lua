626ebd8c8b5e8de7bb2657146c552cca3691b4fc
Pyroman <pyroman@the-ugc.com>
2016-09-14 18:54:43 +0300

    Changed overrides

--- lua/advdoors/hud/cl_overrides.lua
+++ lua/advdoors/hud/cl_overrides.lua
@@ -1,6 +1,8 @@
 --Ugly workaround for the door menu removal.
 hook.Add("onKeysMenuOpened", "AdvancedDoorSystem_F2Override", function(ent, panel)
-	panel:Remove()
+	if ent:isDoor() then
+		panel:Remove()
+	end
 end)
 hook.Add("HUDDrawDoorData", "AdvancedDoorSystem_DrawDoorOverride", function(self)
 	return true
