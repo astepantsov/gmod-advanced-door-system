d10a1f715f79aa05f1d889fb20a7dc09cf589e49
Pyroman <pyroman@the-ugc.com>
2016-09-15 16:59:03 +0300

    Some fixes

--- lua/advdoors/sh_custom.lua
+++ lua/advdoors/sh_custom.lua
@@ -1,5 +1,27 @@
 -- AdvDoors.SetModificationsDisabled()
 
+local doorbell, reinforce, alarm
+
+if CLIENT then
+
+	AdvDoors.DownloadMaterial("http://i.imgur.com/4ZlW2gE.png", function(self) doorbell = self end) -- Icon made by http://www.flaticon.com/authors/madebyoliver from www.flaticon.com
+
+	AdvDoors.DownloadMaterial("http://i.imgur.com/iGgYdyV.png", function(self) reinforce = self end) -- Icon made by http://www.flaticon.com/authors/freepik from www.flaticon.com
+
+	AdvDoors.DownloadMaterial("http://i.imgur.com/8SThKBP.png", function(self) alarm = self end) -- Icon made by http://www.flaticon.com/authors/trinh-ho from www.flaticon.com
+
+	AdvDoors.useBell = function(door)
+
+		net.Start("advdoors_doorbell")
+
+		net.WriteEntity(door)
+
+		net.SendToServer()
+
+	end
+
+end
+
 AdvDoors.AddModification(
 	ADVDOORS_MODIFICATION_DOORBELL,
 	{
